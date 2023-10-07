30719ac0398f01e8762ca1a34e7adb318185ea2f
Olga Bagrova <o-bagrova@mail.ru>
2023-10-08 01:55:10 +0300

    Add constants for functions

---
+++ modules/run_dna_rna_tools.py
@@ -0,0 +1,32 @@
+DNA = {'A', 'T', 'G', 'C'}
+RNA = {'A', 'U', 'G', 'C'}
+TRANSCRIPTION = {
+    'A': 'A',
+    'T': 'U',
+    'G': 'G',
+    'C': 'C',
+    'a': 'a',
+    't': 'u',
+    'g': 'g',
+    'c': 'c'
+}
+COMPLEMETATION_RNA = {
+    'A': 'U',
+    'U': 'A',
+    'G': 'C',
+    'C': 'G',
+    'a': 'u',
+    'u': 'a',
+    'g': 'c',
+    'c': 'g'
+}
+COMPLEMETATION_DNA = {
+    'A': 'T',
+    'T': 'A',
+    'G': 'C',
+    'C': 'G',
+    'a': 't',
+    't': 'a',
+    'g': 'c',
+    'c': 'g'
+}
