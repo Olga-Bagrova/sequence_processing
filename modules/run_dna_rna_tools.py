95b492d0eaa43cada52762781c57bf411b798dee
Olga Bagrova <o-bagrova@mail.ru>
2023-10-08 01:56:57 +0300

    Add transcribe function

--- modules/run_dna_rna_tools.py
+++ modules/run_dna_rna_tools.py
@@ -54,3 +54,20 @@ def is_rna(seq: str)->bool:
     """
     seqset = set(seq.upper())
     return (seqset <= RNA)
+
+
+def transcribe(seq: str)->str:
+    """
+    Transcribe the DNA-sequence
+    arguments:
+        - seq (str): DNA-sequence for transcription
+    return:
+        - str: transcribed DNA-sequence
+    """
+    res = ''
+    if is_dna(seq):
+        res = ''.join([TRANSCRIPTION[nucleotide] for nucleotide in seq])
+    else:
+        raise ValueError('Only DNA-sequence are able to be transcribed, please entry DNA-sequence')
+    return res
+
