dcbb21b0dfc1f92bd92d6e569dc98669f5b0be6f
Olga Bagrova <o-bagrova@mail.ru>
2023-10-08 01:46:41 +0300

    Add is_length_enough function

--- modules/filter_fastq.py
+++ modules/filter_fastq.py
@@ -14,3 +14,18 @@ def is_gc_enough(seq: str, gc_bounds: tuple)->bool:
         return False
 
 
+def is_length_enough(seq: str, length_bounds: tuple)->bool:
+    """
+    Check whether the length of the sequence satisfies the boundaries
+    arguments:
+        - seq (str): sequence for checking
+        - length_bounds (tuple): lower and upper borders for length of sequence
+    return:
+        - bool: True or False depending on the sufficiency of the length of sequence
+    """ 
+    if (len(seq) >= length_bounds[0]) and (len(seq) <= length_bounds[1]):
+        return True
+    else:
+        return False
+
+
