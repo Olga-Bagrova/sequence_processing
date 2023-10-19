e4f2e960a9beb142fa46a00aa8d1b2a182392fa1
Olga Bagrova <o-bagrova@mail.ru>
2023-10-19 14:53:12 +0300

    Add read_gbk_to_list and extract_genes functions

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -32,3 +32,64 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = '')
                 output_file.write(seq)
 
 convert_multiline_fasta_to_oneline('example_data/example_multiline_fasta.fasta.txt', 'uxotdput')
+
+
+def read_gbk_to_list(input_gbk):
+    with open(input_gbk) as file:
+        line = file.readline()
+        while not line.startswith('FEATURES'):
+            line = file.readline()
+        file.readline()
+        line = file.readline()
+        info_start = ''
+        for char in line:
+            if not char.isalpha():
+                info_start += char
+            else:
+                break
+        genes = []
+        while True:
+            if not line:
+                break
+            gene_info = []
+            while line.startswith(info_start):
+                info_line = line.rsplit(info_start)[1]
+                if info_line.endswith('"\n') or info_line[-2].isnumeric():
+                    gene_info.append(info_line.rstrip())
+                else:
+                    line = file.readline()
+                    while not (line.endswith('"\n') or line[-1].isnumeric()):
+                        info_line = info_line.rstrip()
+                        info_line = info_line + line.rsplit(info_start[:-1])[1]
+                        line = file.readline()
+                        if not line:
+                            break
+                    gene_info.append(info_line.rstrip())
+                line = file.readline()
+            line = file.readline()
+            genes.append(gene_info)
+    return(genes)
+
+
+def extract_genes(gbk_genes_info, inp_genes, n_before=1, n_after=1):
+    genes_to_file = []
+    if not isinstance(inp_genes, list):
+        genes = [inp_genes]
+    for gene in genes:
+        gene = 'gene="' + gene + '"'
+        matching = [gbk_genes_info.index(gene_info) for gene_info in gbk_genes_info if gene in gene_info]
+        print(matching)
+    for matched_index in matching:
+        if matched_index-n_before <0:
+            start = 0
+        else: start = matched_index-n_before
+        if (matched_index+n_after+1) > len(gbk_genes_info):
+            end = len(gbk_genes_info)
+        else:
+            end = matched_index+n_after+1
+        for i in range (start, end):
+            genes_to_file.append(gbk_genes_info[i])
+
+def select_genes_from_gbk_to_fasta(input_gbk, genes, n_before=1, n_after=1, output_fasta = ''):
+    gbk_genes_info = read_gbk_to_list(input_gbk)
+    extracted_genes = extract_genes(gbk_genes_info, genes, n_before=1, n_after=1)
